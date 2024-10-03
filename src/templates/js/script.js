b2a331b018b73fed6f64fb2e228cecbf4624ad27
xhackerfinnx <hfalex120@mail.ru>
2024-10-03 21:19:38 +0300

    перенос style и фотографии в static

--- src/templates/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('nav a');
-
-    links.forEach(link => {
-        link.addEventListener('click', function() {
-            alert(`Вы перешли на раздел: ${this.textContent}`);
-        });
-    });
-});
