18ec0a0973b037a22d22c64a0521753331c7c0d7
xhackerfinnx <hfalex120@mail.ru>
2024-10-03 21:18:33 +0300

    добавил secret_auth

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
+from fastapi.security import OAuth2PasswordBearer
 
 class Settings(BaseSettings):
     POSTGRESQL_USER: SecretStr
@@ -7,6 +8,8 @@ class Settings(BaseSettings):
     POSTGRESQL_HOST: SecretStr
     POSTGRESQL_PORT: SecretStr
     POSTGRESQL_DATABASE: SecretStr
+    SECRET_AUTH: SecretStr
+    SECRET_RESET_VERIF: SecretStr
     
     model_config: SettingsConfigDict = SettingsConfigDict(
         env_file='.env',
