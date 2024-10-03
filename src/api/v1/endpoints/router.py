d2ed6c6e1d81b05dd5b1d09623b8969691a24a90
xhackerfinnx <hfalex120@mail.ru>
2024-10-03 21:18:02 +0300

    добавил базу данных для проверки существует ли пользователь и оптимизировал info

--- src/api/v1/endpoints/router.py
+++ src/api/v1/endpoints/router.py
@@ -1,23 +1,26 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends, HTTPException, status
 from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from db.session import DataBaseUsers
 
-from db.models import DataBaseUsers
 router = APIRouter(
     prefix="",
     tags=["Place"]
 )
 
+
 templates = Jinja2Templates(directory=r"./templates")
 
-@router.get("/")
+@router.get("/", response_class=HTMLResponse)
 async def get_main_place(request: Request):
     
-    flag_users = await DataBaseUsers.sql_check_users(DataBaseUsers)
+    if 'user' not in request.session:
+        print(HTTPException(status_code=403, detail="Необходима аутентификация"))
+        return templates.TemplateResponse(r"authorization.html", {"request": request})
+
+    if await DataBaseUsers.user_check(DataBaseUsers, int(request.session['user'])):
+        await DataBaseUsers.user_add_login_id(DataBaseUsers, int(request.session['user']))
     
     return templates.TemplateResponse(r"index.html", {"request": request})
-
-
-@router.get("/style/style.css")
-async def get_style_place(request: Request):
-    
-    return True
--- src/api/v1/endpoints/router_info.py
+++ src/api/v1/endpoints/router_info.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 router = APIRouter(
     prefix="",
@@ -8,12 +9,11 @@ router = APIRouter(
 
 templates = Jinja2Templates(directory=r"./templates")
 
-@router.get("/info")
+@router.get("/info", response_class=HTMLResponse)
 async def get_profile_place(request: Request):
     
-    return templates.TemplateResponse(r"info.html", {"request": request})
-
-@router.get("/style/style_info.css")
-async def get_style_place(request: Request):
+    if 'user' not in request.session:
+        print(HTTPException(status_code=403, detail="Необходима аутентификация"))
+        return templates.TemplateResponse(r"authorization.html", {"request": request})
     
-    return True
+    return templates.TemplateResponse(r"info.html", {"request": request})
